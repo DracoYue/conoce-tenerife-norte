0c1752ffcb62de0292fac21a3be5f091a292e3e3
Madelaine Martin <made_2503@hotmail.com>
2013-11-19 09:15:49 -0500

    Sendero BBDD

--- ctn/contenido/views.py
+++ ctn/contenido/views.py
@@ -4,9 +4,16 @@ from django.shortcuts import render_to_response
 
 def listar_municipio(request):
     bbdd = Municipio.objects.all()
+    bbdd2 = Senderos.objects.all()
     municipio = []
+    senderos = []
     for i in bbdd:
         municipio.append(i.Nombre)
     municipio.sort()
-    return render_to_response('senderos/senderos.html',{'municipio':municipio})
+    for i in bbdd2:
+        senderos.append(i.Nombre)
+    senderos.sort()
+    return render_to_response('senderos/senderos.html',{'municipio':municipio, 'senderos':senderos})
+    
+
     
