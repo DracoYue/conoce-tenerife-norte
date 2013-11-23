f4f69c4b8c39938124ffa1c98e5332a8b4af8448
Angela <angelahdezd@gmail.com>
2013-11-23 13:31:52 +0000

    listando los senderos

--- ctn/contenido/views.py
+++ ctn/contenido/views.py
@@ -4,38 +4,18 @@ from django.shortcuts import render_to_response
 from django import forms
 from django.shortcuts import render_to_response
 
-def listar_sendero(request, numero):
-    bbdd = Municipio.objects.all()
-    municipio = []
-    for i in bbdd:
-        municipio.append(i)
-    municipio.sort()
-
-    numero = int(numero)
-    #numero = Municipio.objects.get(Nombre=nombre).id
-    senderos = []
-    n_senderos = Senderos.objects.count()
-    for i in range(1,n_senderos+1) :
-        print numero
-        print Senderos.objects.get(id=i).MuNom_id
-        if (numero == Senderos.objects.get(id=i).MuNom_id):
-            senderos.append(Senderos.objects.get(id=i).Nombre)
-    senderos.sort()
-
-    
-    return render_to_response('senderos/rutas.html',{'senderos':senderos, 'municipio':municipio})   
-
-
-
 def municipio(request):
-   
-
     bbdd = Municipio.objects.all()
+    bbdd2 = Senderos.objects.all()
+    senderos = []
     municipio = []
+
     for i in bbdd:
         municipio.append(i)
-    municipio.sort()
 
-    context = {'municipio':municipio}
+    for i in bbdd2:
+        senderos.append(i)
+
+    context = {'municipio':municipio, 'senderos':senderos}
     return render_to_response('senderos/senderos.html', context)
     
--- ctn/ctn/urls.py
+++ ctn/ctn/urls.py
@@ -27,9 +27,6 @@ urlpatterns = patterns('',
     url(r'^contact/', TemplateView.as_view(template_name="static_pages/contact.html")),
     url(r'^about/', TemplateView.as_view(template_name="static_pages/about.html")),
     url(r'^help/', TemplateView.as_view(template_name="static_pages/help.html")),
-    #url(r'^senderos/', 'contenido.views.listar_municipio'),
-    url(r'^rutas/(\d+)/','contenido.views.listar_sendero'),
-    #Senderos
     url(r'^brena/', TemplateView.as_view(template_name="senderos/brena.html")),
   
     url(r'^senderos/', 'contenido.views.municipio'),
