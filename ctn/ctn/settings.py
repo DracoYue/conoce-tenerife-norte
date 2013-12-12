f237fb45f48f1c80863c29f33cc20e79b2a39462
Angela <angelahdezd@gmail.com>
2013-12-12 19:28:43 +0000

    probando con heroku

--- ctn/ctn/settings.py
+++ ctn/ctn/settings.py
@@ -2,9 +2,6 @@
 import os
 import django
 
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -256,3 +253,25 @@ SOCIAL_AUTH_PIPELINE = (
 )
 
 SOCIAL_AUTH_FORCE_POST_DISCONNECT = True
+
+
+#heroku
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
