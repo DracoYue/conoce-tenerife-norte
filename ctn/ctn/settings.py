fe456b3019d8eef497d06b3efa90f741ef134d7d
Angela <angelahdezd@gmail.com>
2013-12-15 17:07:20 +0000

    fallo

--- ctn/ctn/settings.py
+++ ctn/ctn/settings.py
@@ -23,7 +23,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',#'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'ctn',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'user',
+        'USER': 'angela',
         'PASSWORD': '1234',
         'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432',                      # Set to empty string for default.
