fff98c7ece1d8e8b1b217f798a8e5333b246b113
Angela <angelahdezd@gmail.com>
2013-12-16 14:51:09 +0000

    fallo

--- ctn/ctn/settings.py
+++ ctn/ctn/settings.py
@@ -34,8 +34,7 @@ DATABASES = {
 #heroku
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-#default='postgres://user:1234@localhost/ctn'
+DATABASES['default'] =  dj_database_url.config(default='postgres://user:1234@localhost/ctn')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
